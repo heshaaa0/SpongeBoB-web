2728b268730702320789567209325205ff9b3de0
heshaaa5 <heshaedmon@gmail.com>
2024-05-14 10:41:10 +0530

    implemented header

---
+++ app/components/AddToCart.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function AddToCart() {
+    
+  return (
+    <div><button className='btn btn-primary'>Add To Cart</button></div>
+  )
+}
+
+export default AddToCart
---
+++ app/users/page.tsx
@@ -0,0 +1,33 @@
+import { link } from 'fs';
+import React from 'react'
+interface User{
+    id: number;
+    name: string;
+    email:string;
+}
+
+const UserPage = async () => {
+    const res = await fetch ('https://jsonplaceholder.typicode.com/users');
+    const users: User[] =await res.json();
+    return (
+    <>
+    <div>Users</div>
+    <p>{new Date().toLocaleTimeString()}</p>
+    <table className='table table-bordered'>
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Email</th>
+            </tr>
+        </thead>
+        {users.map(user=><tr key={user.id}>
+        <td>{user.name}</td>
+        <td>{user.email}</td>
+        </tr>)}
+    </table>
+    </>
+   
+  )
+}
+
+export default UserPage
