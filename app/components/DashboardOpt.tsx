ee38efb006f8072f1b479449b637a6aa424d429f
heshaaa5 <heshaedmon@gmail.com>
2024-05-19 21:55:07 +0530

    implemented the dashboard option selecting component

--- app/components/DashboardOpt.tsx
+++ app/components/DashboardOpt.tsx
@@ -1,14 +1,24 @@
-import React from "react";
+import React from 'react'
+import Gallery02 from '/public/images/stock/gallery-02.jpg'
+import Image from 'next/image';
 
 const DashboardOpt = () => {
-    
   return (
-    <div>
-      <div>
-        <div>
-          <h3>Dashboard Option 1</h3>
-          <p>Dashboard Option Title</p>
-          <p>
+    <div className='px-36'>
+      <div className="flex flex-row">
+        <Image
+          src={Gallery02}
+          className=""
+          width={500}
+          height={500}
+          priority
+          alt="Image of solution"
+          sizes="(min-width: 1536px) 40rem, (min-width: 1280px) 36rem, (min-width: 768px) 33.33vw"
+        />
+        <div className='pl-20 w-2/4'>
+          <h3 className='text-sm py-5'>Dashboard Option 1</h3>
+          <p className='text-2xl pb-3'>Dashboard Option Title</p>
+          <p className='text-justify'>
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia
             totam aut hic a recusandae, aperiam eaque asperiores aliquid dicta
             blanditiis veritatis, voluptate unde iste reiciendis tenetur maxime
@@ -16,20 +26,29 @@ const DashboardOpt = () => {
           </p>
         </div>
       </div>
-      <div>
-        <div>
-          <h3>Dashboard Option 1</h3>
-          <p>Dashboard Option Title</p>
-          <p>
+      <div className="flex flex-row py-10">
+        <div className='pr-20 w-2/4'>
+          <h3 className='text-sm py-5'>Dashboard Option 1</h3>
+          <p className='text-2xl pb-3'>Dashboard Option Title</p>
+          <p className='text-justify'>
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia
             totam aut hic a recusandae, aperiam eaque asperiores aliquid dicta
             blanditiis veritatis, voluptate unde iste reiciendis tenetur maxime
             incidunt mollitia excepturi.
           </p>
         </div>
+        <Image
+          src={Gallery02}
+          className=""
+          height={500}
+          width={500}
+          priority
+          alt="Image of solution"
+          sizes="(min-width: 1536px) 40rem, (min-width: 1280px) 36rem, (min-width: 768px) 33.33vw"
+        />
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default DashboardOpt;
+export default DashboardOpt
